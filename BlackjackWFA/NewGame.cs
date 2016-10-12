9b1d5c67d86f0b1239ae2da833e28ab8858cc1ce
J S <lamierus@gmail.com>
2016-10-12 14:36:58 -0400

    attempting to fix some things.

---
+++ BlackjackWFA/NewGame.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Engine;
+
+namespace BlackjackWFA {
+    public partial class NewGame : Form {
+        public int ReturnNumDecks { get; set; }
+        public BJPlayer ReturnPlayer { get; set; }
+        private BindingList<int> NumDecksList = new BindingList<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        public NewGame(BJPlayer player) {
+            InitializeComponent();
+
+            cboxDecks.DataSource = NumDecksList;
+            cboxDecks.SelectedIndex = NumDecksList.IndexOf(5);
+
+            ReturnPlayer = player;
+            txtName.Text = ReturnPlayer.Name;
+        }
+
+        /// <summary>
+        ///     
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void txtName_TextChanged(object sender, EventArgs e) {
+            ReturnPlayer.Name = txtName.Text;
+        }
+
+        /// <summary>
+        ///     
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void cboxDecks_DropDownClosed(object sender, EventArgs e) {
+            ReturnNumDecks = (int)cboxDecks.SelectedItem;
+        }
+
+        /// <summary>
+        ///     
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnQuit_Click(object sender, EventArgs e) {
+            DialogResult = DialogResult.Cancel;
+            Close();
+        }
+
+        /// <summary>
+        ///     
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnDeal_Click(object sender, EventArgs e) {
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+    }
+}
